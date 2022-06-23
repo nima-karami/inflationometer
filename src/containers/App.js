9366feac8b8d48642f2d4005b18a9750497c6c17
Nima Karami <94768955+nima-karami@users.noreply.github.com>
2022-06-23 17:19:51 -0400

    adding mui icons

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { Drawer, Toolbar, Box, AppBar, List, Typography, Divider, IconButton, Badge, Container, Grid, Paper, Link } from '@mui/material/';
+import { Menu, ChevronLeft, Notifications } from '@mui/icons-material/';
 import Chart from '../components/Chart';
 import ControlPanel from '../components/ControlPanel';
 import Copyright from '../components/Copyright';
@@ -17,7 +21,6 @@ function App() {
       <body className="App-body">
         <ControlPanel/>        
         <Chart/>
-        
       </body>
       <Copyright/>
     </div>
